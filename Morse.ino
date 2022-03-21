f8f2bc5669b535318dcf6d3ceaadc557b15b193e
Cortex Hub Accelerator <98285190+CortexHub@users.noreply.github.com>
2022-03-21 19:33:16 +0200

    Add files via upload
    Hello commit

---
+++ Morse.ino
@@ -0,0 +1,54 @@
+int dit = 250;
+int dah = 500;
+int longD = 1000;
+int shortD = 150;
+int myMpin = 10;
+
+
+void setup(){
+  pinMode(myMpin,OUTPUT);
+  }
+
+void loop(){
+  digitalWrite(myMpin, HIGH);  
+  delay(dit);
+  digitalWrite(myMpin, LOW);
+  delay(dit);
+  digitalWrite(myMpin, HIGH);
+  delay(dit);
+  digitalWrite(myMpin, LOW);
+  delay(dit);
+  digitalWrite(myMpin, HIGH);
+  delay(dit);
+  digitalWrite(myMpin, LOW);
+  delay(longD);
+  
+
+  digitalWrite(myMpin, HIGH);
+  delay(dah);
+  digitalWrite(myMpin, LOW);
+  delay(dah);
+  digitalWrite(myMpin, HIGH);
+  delay(dah);
+  digitalWrite(myMpin, LOW);
+  delay(dah);
+  digitalWrite(myMpin, HIGH);
+  delay(dah);
+  digitalWrite(myMpin, LOW);
+  delay(longD);
+
+
+  digitalWrite(myMpin, HIGH);
+  delay(dit);
+  digitalWrite(myMpin, LOW);
+  delay(dit);
+  digitalWrite(myMpin, HIGH);
+  delay(dit);
+  digitalWrite(myMpin, LOW);
+  delay(dit);
+  digitalWrite(myMpin, HIGH);
+  delay(dit);
+  digitalWrite(myMpin, LOW);
+  delay(longD);
+
+  }
---
+++ sketch_dec03a.ino
@@ -0,0 +1,55 @@
+int dit = 250;
+int dah = 500;
+int longD = 1000;
+int shortD = 150;
+int myMpin = 10;
+
+
+void setup(){
+  pinMode(myMpin,OUTPUT);
+  }
+
+void hello(){
+  digitalWrite(myMpin, HIGH);
+  
+//  delay(dit);
+//  digitalWrite(myMpin, LOW);
+//  delay(dit);
+//  digitalWrite(myMpin, HIGH);
+//  delay(dit);
+//  digitalWrite(myMpin, LOW);
+//  delay(dit);
+//  digitalWrite(myMpin, HIGH);
+//  delay(dit);
+//  digitalWrite(myMpin, LOW);
+//  delay(longD);
+//  
+//
+//  digitalWrite(myMpin, HIGH);
+//  delay(dah);
+//  digitalWrite(myMpin, LOW);
+//  delay(dah);
+//  digitalWrite(myMpin, HIGH);
+//  delay(dah);
+//  digitalWrite(myMpin, LOW);
+//  delay(dah);
+//  digitalWrite(myMpin, HIGH);
+//  delay(dah);
+//  digitalWrite(myMpin, LOW);
+//  delay(longD);
+//
+//
+//  digitalWrite(myMpin, HIGH);
+//  delay(dit);
+//  digitalWrite(myMpin, LOW);
+//  delay(dit);
+//  digitalWrite(myMpin, HIGH);
+//  delay(dit);
+//  digitalWrite(myMpin, LOW);
+//  delay(dit);
+//  digitalWrite(myMpin, HIGH);
+//  delay(dit);
+//  digitalWrite(myMpin, LOW);
+//  delay(longD);
+
+  }
